724c0cb05ae52ac273a4d3c766d618aabac5f0c5
Michael N. Lipp <mnl@mnl.de>
2017-02-25 16:51:53 +0100

    Docu updates.

--- plantUml8/src/org/jdrupes/taglets/plantUml/package-info.java
+++ plantUml8/src/org/jdrupes/taglets/plantUml/package-info.java
@@ -2,7 +2,8 @@
  * A JDK8 taglet that generates UML diagrams with 
  * [PlantUML](http://plantuml.sourceforge.net/) for inclusion in the javadoc.
  * 
- * [ ![Download](https://api.bintray.com/packages/mnlipp/org.jdrupes/plantuml-taglet/images/download.svg) ](https://bintray.com/mnlipp/org.jdrupes/plantuml-taglet/_latestVersion)
+ * [![Release](https://jitpack.io/v/mnlipp/jdrupes-taglets.svg)](https://jitpack.io/#mnlipp/jdrupes-taglets)
+ * [![Maven Central](https://img.shields.io/maven-central/v/org.jdrupes/plantuml-taglet.svg)](http://search.maven.org/#search%7Cga%7C1%7Cg%3A%22org.jdrupes%22%20AND%20a%3A%22plantuml-taglet%22)
  *  
  * Just use the `@plantUml` tag:
  *
@@ -18,7 +19,7 @@
  * {@literal *}/
  * ```
  *
- * Renders as:
+ * This is rendered as:
  * 
  * ![Example Diagram](example.png)
  * 
@@ -27,6 +28,47 @@
  * compatibility with other tools, like e.g. the 
  * [PlantUML Eclipse Plugin](http://plantuml.com/eclipse) or the
  * [PlantUML IDEA Plugin](https://github.com/esteinberg/plantuml4idea).
+ *
+ * Invoking
+ * --------
+ * 
+ * Specify the taglet on JavaDoc's command line:
+ * 
+ * ```
+ * javadoc -taglet org.jdrupes.taglets.plantUml.Taglet -tagletpath /path/to/org.jdrupes.taglets.plantuml-taglet.jar
+ * ```
+ * 
+ * ### Gradle
+ * 
+ * Configuring a taglet in the javadoc task is not explicitly shown in the
+ * [DSL Reference](https://docs.gradle.org/current/dsl/org.gradle.api.tasks.javadoc.Javadoc.html).
+ * The required properties can be found in the 
+ * [standard otions](https://docs.gradle.org/current/javadoc/org/gradle/external/javadoc/StandardJavadocDocletOptions.html)
+ * 
+ * Here's an example:
+ * 
+ * ```gradle
+ * configurations {
+ *     javadocTaglets
+ * }
+ * 
+ * dependencies {
+ *     javadocTaglets "org.jdrupes:plantuml-taglet:<version>"
+ * }
+ * 
+ * javadoc {
+ * 
+ *     options.tagletPath = configurations.javadocTaglets.files as List
+ *     options.taglets = ["org.jdrupes.taglets.plantUml.Taglet"]
+ *     ...
+ * }
+ * ```
+ * 
+ * The latest version available on maven central is shown in the badge at the
+ * beginning of this page.
+ * 
+ * Notes
+ * -----
  *
  * This taglet is based on the PlantUML integration of
  * [Abnaxos'](https://github.com/Abnaxos) 
