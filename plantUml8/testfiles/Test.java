de424ed42530f5b31be76b3a19dc4168b985a88f
Michael N. Lipp <mnl@mnl.de>
2017-03-01 15:04:23 +0100

    Using special test file without markdown.

---
+++ plantUml8/testfiles/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Description.
+ *
+ * <img src="example.svg" alt="Picture">
+ * <!--
+ * @plantUml example.svg
+ * Alice -> Bob: Authentication Request
+ * Alice <-- Bob: Authentication Response
+ * ' -->
+ **/
+public class Test {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
