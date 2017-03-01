c304fad2ac4e081bf4936d510b65d2b7776e5e30
Michael N. Lipp <mnl@mnl.de>
2017-03-01 15:03:37 +0100

    Improved documentation.

--- plantUml8/src/org/jdrupes/taglets/plantUml/package-info.java
+++ plantUml8/src/org/jdrupes/taglets/plantUml/package-info.java
@@ -11,32 +11,65 @@
  * /**
  *  * Description.
  *  *
- *  * <img src="example.png">
+ *  * <img src="example.svg">
  *  *
- *  * @plantUml example.png
+ *  * This package/class ...
+ *  *
+ *  * <!--
+ *  * @plantUml example.svg
  *  * Alice -> Bob: Authentication Request
- *  * Bob --> Alice: Authentication Response
+ *  * Alice <-- Bob: Authentication Response
+ *  * ' -->
  * {@literal *}/
  * ```
  *
  * This is rendered as:
  * 
- * ![Example Diagram](example.png)
+ * ---
+ * 
+ * Description.
+ * 
+ * ![Example Diagram](example.svg)
+ * 
+ * This package/class ...
+ * 
+ * ---
+ * 
+ * Note the carefully crafted "escapes". The usage of "`<`" and "`>`" in PlantUML 
+ * make javadoc complain about illegal HTML tokens. Of course, you could use
+ * "`&lt;`" and "`&gt;`" but this reduces the readability of the UML descriptions.
+ * 
+ * So, to work around that, we put the PlantUML tag with the description in 
+ * an HTML comment (from "`<!--`" to "`-->`"). In order to make sure that
+ * the closing "`-->`" isn't interpreted by PlantUML, we make it a PlantUML 
+ * comment. You also have to avoid all "`-->`" arrows in your PlantUML description
+ * as this would terminate the HTML comment prematurely. Luckily, this isn't too 
+ * hard because you can always exchange the left and right side of such a relation.  
  * 
  * It's also possible to use `@startuml` and `@enduml` instead, as usual. `@startuml` is
  * simply a synonym for `@plantUml` and `@enduml` will be ignored entirely. Use this for
  * compatibility with other tools, like e.g. the 
  * [PlantUML Eclipse Plugin](http://plantuml.com/eclipse) or the
  * [PlantUML IDEA Plugin](https://github.com/esteinberg/plantuml4idea).
+ * 
+ * Actually, using `@startuml` and `@enduml` makes escaping look a bit less weird:
+ * 
+ * ```
+ * /**
+ *  * <!--
+ *  * @startuml example.svg
+ *  * Alice -> Bob: Authentication Request
+ *  * Alice <-- Bob: Authentication Response
+ *  * @enduml
+ *  * -->
+ * {@literal *}/
+ * ```
+ * 
+ * As the closing "`-->`" is now the text passed to the `@enduml` tag
+ * (which ignores it), it needn't be marked as a PlantUML comment any more.
  *
  * Invoking
  * --------
- * 
- * Specify the taglet on JavaDoc's command line:
- * 
- * ```
- * javadoc -taglet org.jdrupes.taglets.plantUml.Taglet -tagletpath /path/to/org.jdrupes.taglets.plantuml-taglet.jar
- * ```
  * 
  * ### Gradle
  * 
@@ -67,6 +100,22 @@
  * The latest version available on maven central is shown in the badge at the
  * beginning of this page.
  * 
+ * ### Command line
+ * 
+ * Specify the taglet on JavaDoc's command line:
+ * 
+ * ```
+ * javadoc -taglet org.jdrupes.taglets.plantUml.Taglet -tagletpath /path/to/plantuml-taglet.jar:/path/to/plantuml.jar
+ * ```
+ * 
+ * This could be simplified by providing a "fat jar", but I doubt that anybody would
+ * really use it.
+ * 
+ * 
+ * ### Maven
+ * 
+ * Still using this? Well, you're on your own...
+ * 
  * Notes
  * -----
  *
@@ -82,7 +131,7 @@
  * This taglet is released under the
  * [GPL 3.0](http://www.gnu.org/licenses/gpl-3.0-standalone.html).
  *  
- * @plantUml example.png
+ * @plantUml example.svg
  * Alice -> Bob: Authentication Request
  * Bob --> Alice: Authentication Response
  */
