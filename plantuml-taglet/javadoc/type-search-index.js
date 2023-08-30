2a02b0f04612ae1dfb4e94ae64846268124ea891
Michael N. Lipp <mnl@mnl.de>
2023-08-30 13:06:33 +0200

    Update.

--- plantuml-taglet/javadoc/type-search-index.js
+++ plantuml-taglet/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.jdrupes.taglets.plantUml","l":"EndUml"},{"p":"org.jdrupes.taglets.plantUml","l":"PlantUml"},{"p":"org.jdrupes.taglets.plantUml","l":"StartUml"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.jdrupes.taglets.plantUml","l":"EndUml"},{"p":"org.jdrupes.taglets.plantUml","l":"PlantUml"},{"p":"org.jdrupes.taglets.plantUml","l":"StartUml"}];updateSearchResults();
